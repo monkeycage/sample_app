fd486bd35198103a2946c5b86796670b656b5f1a
Hetal Shah <hetal@ireality.co.uk>
2013-03-21 10:11:36 +0000

    Final signup tidy up

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ before_save { email.downcase! }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
   	uniqueness: { case_sensitive: false }
 
-  	validates :password, presence: true, length: { minimum: 6 }
+  	validates :password, length: { minimum: 6 }
   	validates :password_confirmation, presence: true
 
 end
